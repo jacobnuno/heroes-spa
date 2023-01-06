ccd7af78e66d77ff71e0f93d57c9102067ee312c
Jacob Nuno <jacobcnda@gmail.com>
2023-01-05 18:59:07 -0600

    chore: rename main component name

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { HeroesApp } from './HeroesApp';
 
 ReactDOM.createRoot( document.getElementById( 'root' ) ).render(
     <React.StrictMode>
-        <App />
+        <HeroesApp />
     </React.StrictMode>
 );
