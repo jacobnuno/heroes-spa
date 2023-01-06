bebc23bf15c7073d9fa879add34e4981e85bec26
Jacob Nuno <jacobcnda@gmail.com>
2023-01-05 21:44:08 -0600

    feat: login page

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,5 +1,26 @@
+import { useNavigate } from 'react-router-dom';
+
 export const LoginPage = () => {
+
+    const navigate = useNavigate();
+
+    const onLogin = () => {
+        navigate( '/', {
+            replace: true // Avoids the users to go back
+        } );
+    }
+
     return (
-        <h1>LoginPage</h1>
+        <div className="container">
+            <h1>LoginPage</h1>
+            <hr />
+
+            <button
+                className="button is-primary"
+                onClick={ onLogin }
+            >
+                Login
+            </button>
+        </div>
     );
 };
