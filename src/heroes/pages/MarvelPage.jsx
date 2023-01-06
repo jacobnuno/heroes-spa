57f282fcf573d536d466b57ae8d79dd7d598487b
Jacob Nuno <jacobcnda@gmail.com>
2023-01-05 21:45:28 -0600

    feat: add HeroList component to Marvel page

--- src/heroes/pages/MarvelPage.jsx
+++ src/heroes/pages/MarvelPage.jsx
@@ -1,5 +1,9 @@
+import { HeroList } from '../components';
+
 export const MarvelPage = () => {
     return (
-        <h1>MarvelPage</h1>
+        <>
+            <HeroList publisher="Marvel Comics" />
+        </>
     );
 };
