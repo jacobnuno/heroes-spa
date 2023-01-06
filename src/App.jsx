78a3307d453b344b3c58bfa78cdabde3e0728791
Jacob Nuno <jacobcnda@gmail.com>
2023-01-05 18:24:27 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+export const App = () => {
+    return (
+        <div className="App">
+            <h1>Heroes SPA</h1>
+        </div>
+    );
+};
