9d9724569f088998cdb980e9405cd6d92269da28
Jacob Nuno <jacobcnda@gmail.com>
2023-01-13 14:37:59 -0600

    feat: implement HeroCard component

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -1,9 +1,18 @@
+import { Link } from 'react-router-dom';
+
+const CharactersByHero = ({ alter_ego, characters }) => {
+    if ( alter_ego !== characters ) {
+        return (
+            <p>{ characters }</p>
+        );
+    }
+};
+
 export const HeroCard = ({
     id,
     superhero,
-    publisher,
     alter_ego,
-    first_apprearance,
+    first_appearance,
     characters
 }) => {
 
@@ -15,17 +24,25 @@ export const HeroCard = ({
                 <div className="columns">
                     <div className="column is-4">
                         <div className="card-image">
-                            <figure className="image is-4by3">
+                            <figure>
                                 <img src={ heroImageUrl } alt={ superhero } />
                             </figure>
                         </div>
                     </div>
                     <div className="column is-8">
                         <div className="card-content">
-                            <p class="title is-4">{ superhero }</p>
-                            <p class="subtitle is-6">{ alter_ego }</p>
+                            <p className="title is-4">{ superhero }</p>
+                            <p className="subtitle is-6">{ alter_ego }</p>
 
-                            <p></p>
+                            <CharactersByHero alter_ego={ alter_ego } characters={ characters } />
+
+                            <p className="">
+                                <small>{ first_appearance }</small>
+                            </p>
+
+                            <Link to={ `/hero/${ id }` }>
+                                More info...
+                            </Link>
                         </div>
                     </div>
                 </div>
